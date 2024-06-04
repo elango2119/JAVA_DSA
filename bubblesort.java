8de8a1b6e2337613b40f4722b6bb5ad6d3615707
elango2119 <elangokannan24@gmail.com>
2024-06-04 12:23:54 +0530

    bubblesort added

---
+++ bubblesort.java
@@ -0,0 +1,22 @@
+package JAVA_DSA;
+
+public class bubblesort {
+    public static void main(String[] args) {
+        int[] arr = { -1, 0, 9, 11, -8, 80 };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    // Swap
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
